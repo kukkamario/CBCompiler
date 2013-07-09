13ba853362a66a0b555ba7c3aa50dd125e580b91
Lassi Hämäläinen <Latexi95@gmail.com>
2013-07-09 20:41:43 +0300

    Implemented CBFunction constructor and CBFunction::generateFunction.

--- Compiler/cbfunction.cpp
+++ Compiler/cbfunction.cpp
@@ -2,29 +2,50 @@
 #include "scope.h"
 #include "variablesymbol.h"
 #include "value.h"
+#include "runtime.h"
+#include "builder.h"
 
-CBFunction::CBFunction(const QString &name, ValueType *retValue, const QList<CBFunction::Parametre> &params, int line, QFile *file):
+CBFunction::CBFunction(const QString &name, ValueType *retValue, const QList<CBFunction::Parameter> &params, Scope *scope, int line, QFile *file):
 	Function(name, file, line),
 	mParams(params),
-	mScope(new Scope(name)),
-	mBlock(0){
-
+	mScope(scope) {
 	mReturnValue = retValue;
-	for (QList<Parametre>::ConstIterator i = mParams.begin(), end = mParams.end(); i != end; ++i) {
-		mParamTypes.append(i->mType);
-
-		//Parametre variable symbols
-		mScope->addSymbol(new VariableSymbol(i->mName, i->mType, mFile, mLine));
+	mRequiredParams = 0;
+	foreach(const Parameter &param, mParams) {
+		if (!param.mDefaultValue.isValid()) mRequiredParams++;
+		mParamTypes.append(param.mVariableSymbol->valueType());
 	}
-
 }
 
-bool CBFunction::generateFunction(const ast::FunctionDefinition *funcDef) {
-	return true;
+void CBFunction::generateFunction(Runtime *runtime) {
+	std::vector<llvm::Type*> parameterTypes;
+	foreach(const Parameter &param, mParams) {
+		parameterTypes.push_back(param.mVariableSymbol->valueType()->llvmType());
+	}
+	llvm::FunctionType *funcTy = llvm::FunctionType::get(mReturnValue->llvmType(), parameterTypes, false);
+	mFunction = llvm::Function::Create(funcTy, llvm::Function::PrivateLinkage, "CBF_user_" + mName.toStdString(), runtime->module());
 }
 
 
 Value CBFunction::call(Builder *builder, const QList<Value> &params) {
-	assert(0);
-	return Value();
+	std::vector<llvm::Value*> p;
+	QList<Parameter>::ConstIterator paramI = mParams.begin();
+	foreach(const Value &val, params) {
+		p.push_back(builder->llvmValue(val));
+		paramI++;
+	}
+
+	//Default parameters
+	for (int i = params.size(); i < mParams.size(); i++) {
+		p.push_back(builder->llvmValue(paramI->mDefaultValue));
+		paramI++;
+	}
+
+	llvm::Value *ret = builder->irBuilder().CreateCall(mFunction, p);
+	if (isCommand()) {
+		return Value();
+	}
+	else {
+		return Value(mReturnValue, ret);
+	}
 }
--- Compiler/cbfunction.h
+++ Compiler/cbfunction.h
@@ -3,8 +3,8 @@
 #include "function.h"
 #include "constantvalue.h"
 #include <QPair>
-#include "abstractsyntaxtree.h"
 class Scope;
+class VariableSymbol;
 
 /**
  * @brief The CBFunction class
@@ -12,25 +12,22 @@ class Scope;
 
 class CBFunction : public Function {
 	public:
-		struct Parametre {
-				Parametre():mType(0){}
-				QString mName;
-				ValueType *mType;
+		struct Parameter {
+				Parameter() : mVariableSymbol(0) {}
+				VariableSymbol *mVariableSymbol;
 				ConstantValue mDefaultValue;
 		};
 
-		CBFunction(const QString & name, ValueType *retValue, const QList<Parametre> &params, int line, QFile *file);
-		bool generateFunction(const ast::FunctionDefinition *funcDef);
+		CBFunction(const QString & name, ValueType *retValue, const QList<Parameter> &params, Scope *scope, int line, QFile *file);
+		void generateFunction(Runtime *runtime);
 		void setScope(Scope *scope);
-		void setBlock(ast::Block *block) {mBlock = block;}
-		ast::Block *block() const {return mBlock;}
 		Scope *scope() const {return mScope;}
 		bool isRuntimeFunction() const {return false;}
 		Value call(Builder *builder, const QList<Value> &params);
+		const QList<Parameter> &parameters() const { return mParams; }
 	private:
-		QList<Parametre> mParams;
+		QList<Parameter> mParams;
 		Scope *mScope;
-		ast::Block *mBlock;
 };
 
 #endif // CBFUNCTION_H
